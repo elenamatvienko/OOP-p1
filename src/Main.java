6f0aed1c8be2acda3afabf2deb634255cc5999e2
elena <elena.matvienko2011@mail.ru>
2022-12-31 09:54:56 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import transport.Car;
+
 public class Main {
     public static void main(String[] args) {
 
-        Car lada = new Car(null, "Granta", 1.7, "желтый",0, "Россия");
+        Car lada = new Car(null, "Granta", 1.7, "желтый",0, "Россия", "автоматическая", "седан", "а001аа61", 4, true, true, true);
 //        lada.brand = "Lada ";
 //        lada.model = "Granta";
 //        lada.engineVolume = 1.7;
@@ -9,7 +11,7 @@ public class Main {
 //        lada.year = 2015;
 //        lada.country = "Россия";
 
-        Car audi = new Car("Audi", null, 3.0,"черный", 2020, "Германия");
+        Car audi = new Car("Audi", null, 3.0,"черный", 2020, "Германия", "автоматическая", "седан", "а001аа61", 4, true, true, true);
 //        audi.brand = "Audi";
 //        audi.model = "A8 50 L TDI quattro";
 //        audi.engineVolume = 3.0;
@@ -17,7 +19,7 @@ public class Main {
 //        audi.year = 2020;
 //        audi.country = "Германия";
 
-        Car bmv = new Car("BMW", "Z8", 3.0, "черный",0,"Германия");
+        Car bmv = new Car("BMW", "Z8", 3.0, "черный",0,"Германия", "автоматическая", "седан", "а001аа61", 4, true, true, true);
 //        bmv.brand = "BMW";
 //        bmv.model = "Z8";
 //        bmv.engineVolume = 3.0;
@@ -25,7 +27,7 @@ public class Main {
 //        bmv.year = 2021;
 //        bmv.country = "Германия";
 
-        Car kia = new Car("Kia", "Sportage 4-го поколения",2.4, null, 2018,"Южная Корея");
+        Car kia = new Car("Kia", "Sportage 4-го поколения",2.4, null, 2018,"Южная Корея", "автоматическая", "седан", "а001аа61", 4, true, true, true );
 //        kia.brand = "Kia";
 //        kia.model = "Sportage 4-го поколения";
 //        kia.engineVolume = 2.4;
@@ -33,7 +35,7 @@ public class Main {
 //        kia.year = 2018;
 //        kia.country = "Южная Корея";
 
-        Car hyundai  = new Car("Hyundai", "Avante",1.6, "оранжевый", 2016, null);
+        Car hyundai  = new Car("Hyundai", "Avante",1.6, "оранжевый", 2016, null, "автоматическая", "седан", "а001аа61", 4, true, true, true);
 //        hyundai.brand = "Hyundai";
 //        hyundai.model = "Avante";
 //        hyundai.engineVolume = 1.6;
